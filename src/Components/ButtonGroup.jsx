70c4b67e2c6dd8fa9884d036847e18f1f37635b6
Qifeng Li <a879265509@hotmail.com>
2023-12-15 10:55:09 -0600

    map

--- src/Components/ButtonGroup.jsx
+++ src/Components/ButtonGroup.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Button from "./Button";
 
+const secondaryText = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+
 const ButtonGroup = () => {
-  const secondaryText = [
-    "Mark all as complete",
-    "Mark all as incomplete",
-    "Reset to initial",
-    "Remove all items",
-  ];
   return (
     <section className="button-group">
+      {/*here we return a [] because it's a map(), but react is good for that*/}
       {secondaryText.map((text) => {
-        return <Button type="secondary" text={text} />;
+        return <Button key={text} type="secondary" text={text} />;
       })}
     </section>
   );
